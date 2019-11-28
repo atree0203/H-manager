d0b77cafc92f8bc05bf08c9d1fbfadb05706250d
xsl <578375797@qq.com>
2019-11-28 15:57:47 +0800

    上传文件

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,8 +1,32 @@
 <template>
 	<div style="padding:30px;">
-		<el-button plain @click="open3">
+		
+		<!-- <el-button plain @click="open3">
 			成功
-		</el-button>
+		</el-button> -->
+		<!-- <el-upload
+			class="avatar-uploader"
+			action="http://ec2-34-244-176-172.eu-west-1.compute.amazonaws.com:2333/source/upload"
+			
+			:show-file-list="false"
+			:on-success="handleAvatarSuccess"
+			:before-upload="beforeAvatarUpload">
+			<img v-if="imageUrl" :src="imageUrl" class="avatar">
+			<i v-else class="el-icon-plus avatar-uploader-icon"></i>
+		</el-upload> -->
+		<el-upload
+		  class="upload-demo"
+		  ref="upload"
+		  :data="datas"
+		  action="http://ec2-34-244-176-172.eu-west-1.compute.amazonaws.com:2333/source/upload"
+		  :on-preview="handlePreview"
+		  :on-remove="handleRemove"
+		  :on-success="uploadSussess"
+		  :file-list="fileList"
+		  :auto-upload="false">
+		  <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+		  <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+		</el-upload>
 		<el-form :inline="true" :model="formInline" class="demo-form-inline">
 			<el-form-item label="审批人">
 				<el-input v-model="formInline.user" placeholder="审批人"></el-input>
@@ -46,6 +70,14 @@
 	export default {
 		data() {
 			return {
+				datas:{
+					filename:'测试xls'
+				},
+				fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}, {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
+				imageUrl: '',
+				headers:{
+					token:'123'
+				},
 				tableData: [{
 					date: '2016-05-02',
 					name: '王小虎',
@@ -71,6 +103,33 @@
 			}
 		},
 		methods: {
+			uploadSussess(response, file, fileList){
+				console.log(response, file, fileList)
+			},
+			submitUpload() {
+			    this.$refs.upload.submit();
+			},
+			handleRemove(file, fileList) {
+			        console.log(file, fileList);
+			},
+			handlePreview(file) {
+			    console.log(file);
+			},
+			handleAvatarSuccess(res, file) {
+				this.imageUrl = URL.createObjectURL(file.raw);
+			},
+			beforeAvatarUpload(file) {
+				const isJPG = file.type === 'image/jpeg';
+				const isLt2M = file.size / 1024 / 1024 < 2;
+
+				if (!isJPG) {
+				this.$message.error('上传头像图片只能是 JPG 格式!');
+				}
+				if (!isLt2M) {
+				this.$message.error('上传头像图片大小不能超过 2MB!');
+				}
+				return isJPG && isLt2M;
+			},
 			handleEdit(index, row) {
 				console.log(index, row);
 			},
@@ -99,3 +158,31 @@
 		}
 	}
 </script>
+<style scoped>
+	.avatar-uploader, el-upload {
+		border: 1px dashed #E1E1E1;
+		border-radius: 6px;
+		cursor: pointer;
+		position: relative;
+		overflow: hidden;
+		/* height: 100px; */
+		width: 200px;
+		margin: 20px;
+	}
+	.avatar-uploader .el-upload:hover {
+		border-color: #409EFF;
+	}
+		.avatar-uploader-icon {
+		font-size: 28px;
+		color: #8c939d;
+		width: 178px;
+		height: 178px;
+		line-height: 178px;
+		text-align: center;
+	}
+	.avatar {
+		width: 178px;
+		height: 178px;
+		display: block;
+	}
+</style>
